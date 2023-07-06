c134c56835fd16403c47c2b7c64cb69714e00e57
Mezz Davies <mezzdavies@gmail.com>
2023-07-06 12:22:26 +0100

    static render

---
+++ fortune-teller/src/components/Main.jsx
@@ -0,0 +1,14 @@
+import "../styles/main.css";
+import Button from "./Button";
+import Fortune from "./Fortune";
+
+export default function Main() {
+  return (
+    <main>
+      <p>this is the main section</p>
+      <Button />
+      <Fortune />
+      <Button />
+    </main>
+  );
+}
