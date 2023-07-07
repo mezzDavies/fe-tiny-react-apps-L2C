6080472dae4e76b765e7cc0e7bfe48edc18213ad
Mezz Davies <mezzdavies@gmail.com>
2023-07-07 14:42:41 +0100

    tidy up main component

--- fortune-teller/src/components/Main.jsx
+++ fortune-teller/src/components/Main.jsx
@@ -1,13 +1,8 @@
 import "../styles/main.css";
 
 import Fortune from "./Fortune";
-import { useState } from "react";
-import setRandomFortune from "../helpers/setRandomFortune.js";
-import importedFortunes from "../../../assets/fortunes";
 
 export default function Main() {
-  //   setRandomFortune(importedFortunes, setCurrentFortune);
-
   return (
     <main>
       <p>Press the button for a deep, insightful glimpse of your future...</p>
